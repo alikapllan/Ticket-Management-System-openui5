f668372cad97d4aacc68dbbf8fe7da0de2d3c72f
alikapllan <alihkaplan44@gmail.com>
2025-05-29 12:34:32 +0200

    replace JS Date Obj. with UI5Date in formatter.js
    -> due to ui5 documentation UI5Date is suggested to be consumed over the standard JS Date object. For the reasons refer to the links below.
    
    sources:
    -> https://sapui5.hana.ondemand.com/#/topic/6c9e61dc157a40c19460660ece8368bc.html#loio6c9e61dc157a40c19460660ece8368bc/section_ui5date
    -> https://sapui5.hana.ondemand.com/#/api/module:sap/ui/core/date/UI5Date

--- tmui5/webapp/model/formatter.js
+++ tmui5/webapp/model/formatter.js
@@ -1,48 +1,51 @@
-sap.ui.define(["sap/ui/core/format/DateFormat"], function (DateFormat) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/format/DateFormat", "sap/ui/core/date/UI5Date"],
+  function (DateFormat, UI5Date) {
+    "use strict";
 
-  return {
-    formatDateTime: function (sDate) {
-      if (!sDate) {
-        return "";
-      }
+    return {
+      formatDateTime: function (sDate) {
+        if (!sDate) {
+          return "";
+        }
 
-      const oDate = new Date(sDate); // Convert ISO string to Date object
+        const oUI5Date = UI5Date.getInstance(sDate); // UI5Date instead of JS Date obj.
 
-      const oDateFormat = DateFormat.getDateTimeInstance({
-        pattern: "dd/MM/yyyy HH:mm",
-      });
+        const oDateFormat = DateFormat.getDateTimeInstance({
+          pattern: "dd/MM/yyyy HH:mm",
+        });
 
-      return oDateFormat.format(oDate);
-    },
+        return oDateFormat.format(oUI5Date);
+      },
 
-    getStatusState: function (sTicketStatusName) {
-      if (!sTicketStatusName) {
-        return this.ValueState.None; // Default state if status is missing
-      }
+      getStatusState: function (sTicketStatusName) {
+        if (!sTicketStatusName) {
+          return this.ValueState.None; // Default state if status is missing
+        }
 
-      const oResourceBundle = this.getOwnerComponent()
-        .getModel("i18n")
-        .getResourceBundle();
+        const oResourceBundle = this.getOwnerComponent()
+          .getModel("i18n")
+          .getResourceBundle();
 
-      switch (sTicketStatusName) {
-        case oResourceBundle.getText("statusNew"):
-          return this.ValueState.Information;
-        case oResourceBundle.getText("statusInPlanning"):
-          return this.ValueState.Warning;
-        case oResourceBundle.getText("statusInProgress"):
-          return this.ValueState.Success;
-        case oResourceBundle.getText("statusWaitingForCustomerFeedback"):
-          return this.ValueState.Warning;
-        case oResourceBundle.getText("statusOnHold"):
-          return this.ValueState.None;
-        case oResourceBundle.getText("statusCanceled"):
-          return this.ValueState.Error;
-        case oResourceBundle.getText("statusDone"):
-          return this.ValueState.Success;
-        default:
-          return this.ValueState.None;
-      }
-    },
-  };
-});
+        switch (sTicketStatusName) {
+          case oResourceBundle.getText("statusNew"):
+            return this.ValueState.Information;
+          case oResourceBundle.getText("statusInPlanning"):
+            return this.ValueState.Warning;
+          case oResourceBundle.getText("statusInProgress"):
+            return this.ValueState.Success;
+          case oResourceBundle.getText("statusWaitingForCustomerFeedback"):
+            return this.ValueState.Warning;
+          case oResourceBundle.getText("statusOnHold"):
+            return this.ValueState.None;
+          case oResourceBundle.getText("statusCanceled"):
+            return this.ValueState.Error;
+          case oResourceBundle.getText("statusDone"):
+            return this.ValueState.Success;
+          default:
+            return this.ValueState.None;
+        }
+      },
+    };
+  }
+);
